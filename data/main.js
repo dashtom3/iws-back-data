b575e80bf06d65659133216e3d14e9bd2462316a
Tian Chengyuan <395750080@qq.com>
2018-07-06 20:28:04 +0800

    dd
    dd

--- data/main.js
+++ data/main.js
@@ -4,92 +4,108 @@
 import formidable from 'formidable'
 import snap7 from 'node-snap7'
 import axios from 'axios'
-const url = "http://localhost:8001/"
+var tcpDataSet = {}
+var modbusDataSet = {}
+const url = "http://localhost:6784/"
 class Main {
-    constructor(tcpDataSet,modbusDataSet) {
-        this.tcpDataSet = tcpDataSet
-        this.modbusDataSet = modbusDataSet
+    constructor() {
+        // tcpDataSet = {}
+        // this.modbusDataSet = {}
     }
     async getAllData(){
-        console.log(url+'allStartSensor')
+        // console.log(url+'allStartSensor')
         var p = await axios.get(url+'data/allStartSensor')
-        console.log(p.data)
+        // console.log(p.data)
         this.analyse(p.data.data)
-        console.log(this.tcpDataSet,this.modbusDataSet)
+        console.log(tcpDataSet,modbusDataSet)
         this.collectData()
     }
     analyse(data){
-        this.modbusDataSet = []
-        this.tcpDataSet = []
+        modbusDataSet = {}
+        tcpDataSet = {}
         data.forEach(item=>{
             if(item.transfer_type == 0) {
-                this.modbusDataSet.push(item)
+                if(modbusDataSet[item.ip]){
+                    modbusDataSet[item.ip].push(item)
+                }else {
+                    modbusDataSet[item.ip] = [item]
+                }
+                
             } else {
-                this.tcpDataSet.push(item)
+                tcpDataSet[item.ip] = item
             }
         })
     }
     collectData(){
-        this.tcpDataSet.forEach(item=>{
-            this.tcpCollectData(item)
-        })
+        for(var i in tcpDataSet) {
+            this.tcpCollectData(tcpDataSet[i])
+        }
     }
     tcpCollectData(item){
         var s7client = new snap7.S7Client()
+        tcpDataSet[item.ip].s7client = s7client
         s7client.ConnectTo(item.ip,0,1,(err)=>{
             if(err)
-                return console.log(err+'-'+s7client.ErrorText)
+                return console.log(s7client.ErrorText)
             var startAddress = item.point.pointEnum[0].place
             var lastAddressNum = item.point.pointEnum.length-1
             var datalength = item.point.pointEnum[lastAddressNum].place-startAddress+item.point.pointEnum[lastAddressNum].placeLength
+            console.log(startAddress,lastAddressNum,datalength)
             s7client.DBRead(1,startAddress,datalength,(err,res)=>{
                 if(err)
-                    return console.log(err+'-'+s7client.ErrorText)
-                console.log(res)
+                    return console.log(s7client.ErrorText)
+                console.log(res.join())
             })
         })
     }
     async startRead(req, res, next){
         console.log(req.body)
-        const form = new formidable.IncomingForm();
-		form.parse(req, async (err, fields, files) => {
-			
-			console.log(fields)  
-        })
-        res.send({
-            status: 0,
-            type: 'FORM_DATA_ERROR',
-            message: '表单信息错误'
-        })
+        var device = req.body
+        try {
+            console.log(Main)
+            console.log(tcpDataSet)
+        //TCP
+            if(device.sensor[0].transfer_type == 1){
+                device.sensor.forEach(item=>{
+                    console.log(tcpDataSet)
+                    // if(tcpDataSet[item.ip]){
+                    //     console.log(tcpDataSet)
+                    //     tcpDataSet[item.ip].s7client.stop()  
+                    // }
+                    tcpDataSet[item.ip] = item
+                    Main.tcpCollectData(item)
+                })
+            }else {
+            //MODEBUS    
+            }
+            res.send({
+                status: 1,
+                message: '开启成功'
+            })
+        } catch (error) {
+            console.log(error)
+        }
+        
     }
     async stopRead(req, res, next){
-    const _id = req.params._id
-    if (!_id) {
-            console.log('参数错误');
-            res.send({
-                status: 0,
-                type: 'ERROR_PARAMS',
-                message: '参数错误',
+        console.log(req.body)
+        var device = req.body
+        //TCP
+        if(device.sensor[0].transfer_type == 1){
+            device.sensor.forEach(item=>{
+                if(tcpDataSet[item.ip]){
+                    // tcpDataSet[item.ip].s7client.stop()
+                    delete tcpDataSet[item.ip]  
+                }
             })
-            return
+        }else {
+        //MODEBUS    
         }
-    try {
-        const device = await DeviceModel.findOne({'_id':_id})
-        
-        await PointModel.findOneAndRemove({'_id':_id})
         res.send({
             status: 1,
-            message: '开启成功'
-        })
-    }catch(err){
-        console.log('删除失败', err);
-        res.send({
-            status: 0,
-            type: 'ERROR_GET_LIST',
-            message: '删除失败'
+            message: '关闭成功'
         })
     }
-}
    
 
 }
